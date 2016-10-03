075a87b4aeecf45553c7f7f3e4a8ff43d38a660e
Robin Millette <robin@millette.info>
2016-10-03 03:10:53 -0400

    Fix mp3 test.

--- test.js
+++ test.js
@@ -2,32 +2,33 @@
 import test from 'ava'
 import fn from './'
 
-const laDate = '2016-10-01'
+const laDate = '2016-10-02'
 
-test('get shows', async t => {
+test.serial('get shows', async t => {
   const result = await fn.getShows(laDate)
   t.is(result.length, 2)
 })
 
-test('get episodes', async t => {
+test.serial('get episodes', async t => {
   const result0 = await fn.getShows(laDate)
   const result = await fn.getEpisodes(result0[0].rss)
   t.is(result.rss.channel.item.length, 11)
 })
 
-test.skip('get mp3', t => {
-// test.cb('get mp3', t => {
+test.cb('get mp3', t => {
   fn.getShows(laDate)
-    .then(a => fn.getEpisodes(a[0].rss))
-    .then(a => {
-      console.log('a...')
-      let s = fn.getMP3(a.rss.channel.item[0].enclosure.url)
+    .then(r0 => fn.getEpisodes(r0[0].rss))
+    .then(r1 => fn.getMP3(r1.rss.channel.item[10].enclosure.url))
+    .then(s => {
       let len = 0
+      t.plan(1)
+      s.on('error', console.log.bind(console, 'error'))
       s.on('end', () => {
-        console.log('end...')
-        t.is(len, 5034736)
+        t.is(len, 4386485)
         t.end()
       })
-      s.on('data', d => { len += d.length })
+      s.on('data', d => {
+        len += d.length
+      })
     })
 })
