d093283eb5708bf3519d767a621440d9b1ff0095
Robin Millette <robin@millette.info>
2016-10-03 01:30:26 -0400

    Fix tests.

--- test.js
+++ test.js
@@ -2,25 +2,29 @@
 import test from 'ava'
 import fn from './'
 
+const laDate = '2016-10-01'
+
 test('get shows', async t => {
-  const result = await fn.getShows('2016-09-19')
-  t.is(result.length, 6)
+  const result = await fn.getShows(laDate)
+  t.is(result.length, 2)
 })
 
 test('get episodes', async t => {
-  const result0 = await fn.getShows('2016-09-19')
+  const result0 = await fn.getShows(laDate)
   const result = await fn.getEpisodes(result0[0].rss)
-  t.is(result.rss.channel.item.length, 17)
+  t.is(result.rss.channel.item.length, 11)
 })
 
 test.skip('get mp3', t => {
 // test.cb('get mp3', t => {
-  fn.getShows('2016-06-24')
+  fn.getShows(laDate)
     .then(a => fn.getEpisodes(a[0].rss))
     .then(a => {
+      console.log('a...')
       let s = fn.getMP3(a.rss.channel.item[0].enclosure.url)
       let len = 0
       s.on('end', () => {
+        console.log('end...')
         t.is(len, 5034736)
         t.end()
       })
